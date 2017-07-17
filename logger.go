881179e08052dd937614cdf1f8a50d76e8dd0355
Brunetto Ziosi <brunetto.ziosi@pixartprinting.com>
2017-07-17 16:55:44 +0200

    resp body log

--- logger.go
+++ logger.go
@@ -8,6 +8,8 @@ import (
 	"github.com/Sirupsen/logrus"
 	"gopkg.in/gin-gonic/gin.v1"
 	"gitlab.com/brunetto/hang"
+	"bytes"
+	"strings"
 )
 
 // 2016-09-27 09:38:21.541541811 +0200 CEST
@@ -16,7 +18,30 @@ import (
 // "http://www.example.com/start.html"
 // "Mozilla/4.08 [en] (Win98; I ;Nav)"
 
-var timeFormat = "2006-01-02T15:04:05-0700"
+var TimeFormat = "2006-01-02T15:04:05-0700"
+
+var RespBodySizeLimit int = 5000
+
+var RespBodyExludedRoutes = []string{"css", "font", "js", "assets", "icons", "img", "images", "script"}
+
+func ExcludeRespBodyLog(path string) bool {
+	for _, item := range RespBodyExludedRoutes {
+		if strings.Contains(path, item) {
+			return true
+		}
+	}
+	return false
+}
+
+type RespBodyLogger struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w RespBodyLogger) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
+}
 
 // Logger is the logrus logger handler
 func Logger(log *logrus.Logger) gin.HandlerFunc {
@@ -24,6 +49,7 @@ func Logger(log *logrus.Logger) gin.HandlerFunc {
 		// before handle actions
 		var (
 			requestBody []byte
+			rbd string
 			err error
 		)
 
@@ -33,6 +59,10 @@ func Logger(log *logrus.Logger) gin.HandlerFunc {
 
 		// Record request
 		requestBody = hang.Tee(&(c.Request.Body))
+
+		// Record response
+		responseBody := &RespBodyLogger{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
+		c.Writer = responseBody
 
 		c.Next() // here handlers are executed
 
@@ -52,6 +82,21 @@ func Logger(log *logrus.Logger) gin.HandlerFunc {
 			responseDataLength = 0
 		}
 
+		if !ExcludeRespBodyLog(path) {
+			if responseDataLength < RespBodySizeLimit {
+				rbd = responseBody.body.String()
+			} else {
+				if responseBody.body.Len() > 0 {
+					responseBody.body.Truncate(RespBodySizeLimit)
+				} else {
+					rbd = "... negative size body... "
+				}
+				rbd = responseBody.body.String() + "... [truncated]"
+			}
+		} else {
+			rbd = "not logged"
+		}
+
 		entry := logrus.NewEntry(log).WithFields(logrus.Fields{
 			"hostname":   hostname,
 			"statusCode": statusCode,
@@ -64,7 +109,10 @@ func Logger(log *logrus.Logger) gin.HandlerFunc {
 			"at": c.Keys["at"],
 			"requestBody": string(requestBody),
 			//"userAgent":  clientUserAgent,
+			"responseBody": rbd,
 		})
+
+
 
 		if len(c.Errors) > 0 {
 			entry.Error(c.Errors.ByType(gin.ErrorTypePrivate).String())
